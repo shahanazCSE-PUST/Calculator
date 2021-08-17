7bd96c01b55b837ea8da3ad623f2aecafb704be4
ShahanazCSE-PUST <shahanaz.parveen94@outlook.com>
2021-08-17 18:41:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const display=document.getElementById('result-id');
+function one(){
+    display.innerText += 1;
+}
+function two(){
+    display.innerText += 2;
+}
+function three(){
+    display.innerText += 3;
+}
+function four(){
+    display.innerText += 4;
+}
+function five(){
+    display.innerText += 5;
+}
+function six(){
+    display.innerText += 6;
+}
+function seven(){
+    display.innerText += 7;
+}
+function eight(){
+    display.innerText += 8;
+}
+function nine(){
+    display.innerText += 9;
+}
+function zero(){
+    display.innerText += 0;
+}
+function dot(){
+    display.innerText += '.';
+}
+function plus(){
+    display.innerText += '+';
+}
+function minus(){
+    display.innerText += '-';
+}
+function multi(){
+    display.innerText += '*';
+}
+function div(){
+    
+    display.innerText += '/';
+}
+function modulo(){
+    display.innerText += '%';
+}
+function clr(){
+    display.innerText = '';
+}
+function equal() {
+    var result = eval(display.innerText);
+    display.innerText='= ' + result;
+
+}
+
+    
